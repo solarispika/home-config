c6dfb57fc0300f10644b89737e84425f5397619e
Sung, Po Han <solarispika@gmail.com>
2021-06-08 16:28:36 +0000

    Keep .zshrc

--- setup-home.sh
+++ setup-home.sh
@@ -8,7 +8,7 @@ command -v git > /dev/null 2>&1 || sudo apt-get install git
 git config --global user.name "Sung, Po Han"
 git config --global user.email "solarispika@gmail.com"
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended || true
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc || true
 
 zsh ./setup-home-after-zsh.sh
 
