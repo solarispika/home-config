33f9577727c80d785549c520cf6ffafe728918de
Sung, Po Han <solarispika@gmail.com>
2021-06-08 17:16:19 +0000

    Add ssh keys

--- .home-setup/install-after-zsh.sh
+++ .home-setup/install-after-zsh.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env zsh
 
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | zsh
 source $HOME/.zshrc
 nvm install stable
@@ -9,3 +8,6 @@ bash <(curl -sL https://git.io/haskell-vim-now) --basic
 ln -sf ~/.home-setup/vimrc.local .config/haskell-vim-now/
 ln -sf ~/.home-setup/vimrc.local.pre .config/haskell-vim-now/
 ln -sf ~/.home-setup/plugins.vim .config/haskell-vim-now/
+
+ssh-keygen -t ed25519 -f $HOME/.ssh/id_ed25519 -N ""
+
