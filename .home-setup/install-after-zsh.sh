1a03a2938eaaf26d36fb4bcbb41a8cd767ab897b
Sung, Po Han <solarispika@gmail.com>
2021-06-09 01:56:16 +0000

    tmux

--- .home-setup/install-after-zsh.sh
+++ .home-setup/install-after-zsh.sh
@@ -11,3 +11,14 @@ ln -sf ~/.home-setup/plugins.vim .config/haskell-vim-now/
 
 ssh-keygen -t ed25519 -f $HOME/.ssh/id_ed25519 -N ""
 
+command -v tmux > /dev/null 2>&1 || sudo apt-get install tmux
+command -v ruby > /dev/null 2>&1 || sudo apt-get install ruby
+sudo gem install tmuxinator
+
+command -v python > /dev/null 2>&1 || sudo apt-get install python3 python-is-python3 python3-pip
+command -v pip > /dev/null 2>&1 || sudo apt-get install python3-pip
+pip install --user powerline-status
+
+git clone https://github.com/gpakosz/.tmux.git
+ln -sf $HOME/.tmux/.tmux.conf $HOME/
+cp $HOME/.tmux/.tmux.conf.local $HOME/
